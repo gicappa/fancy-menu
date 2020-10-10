ad885141af5aba2f930cc5a967f958a527c33607
Gian Carlo Pace <giancarlo.pace@gmail.com>
2020-10-10 11:44:44 +0200

    Added a app class that prints out the menu as an example

--- src/main/java/gk/menu/App.java
+++ src/main/java/gk/menu/App.java
@@ -2,12 +2,19 @@ package gk.menu;
 
 /**
  * Hello world!
- *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(String[] args) {
+        Menu menu = new Menu();
+        menu.addItem(new MenuItemLeaf("item1"));
+        menu.addItem(new MenuItemLeaf("item2"));
+
+        MenuItemFolder folder1 = new MenuItemFolder("folder1");
+        folder1.addItem(new MenuItemLeaf("sub item1"));
+        folder1.addItem(new MenuItemLeaf("sub item2"));
+
+        menu.addItem(folder1);
+
+        System.out.println(menu);
     }
 }
