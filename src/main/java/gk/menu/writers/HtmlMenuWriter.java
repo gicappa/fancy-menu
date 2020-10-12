f6444a6745115f6dcd4d2c76dbab88d37508b614
Gian Carlo Pace <giancarlo.pace@gmail.com>
2020-10-12 17:54:05 +0200

    Refactored to better reveal intention of the HtmlMenuWriter

--- src/main/java/gk/menu/writers/HtmlMenuWriter.java
+++ src/main/java/gk/menu/writers/HtmlMenuWriter.java
@@ -2,38 +2,52 @@ package gk.menu.writers;
 
 import gk.menu.MenuItem;
 import gk.menu.MenuWriter;
+import gk.menu.RootItem;
+
+import static java.util.stream.Collectors.joining;
 
 public class HtmlMenuWriter implements MenuWriter {
 
+
     @Override
     public String writeItem(MenuItem item, int level) {
-        // This a RootMenuItem
-        if (item.getName().isBlank()) {
-            return writeChildren(item, level);
+        if (item instanceof RootItem) {
+            return writeRootItem(item, level);
+        } else if (isLeafItem(item)) {
+            return writeLeafItem(item);
+        } else {
+            return writeMenuItem(item, level);
         }
+    }
 
-        // This a normal Container Menu Item or a Leaf Menu Item
+    public String writeRootItem(MenuItem item, int level) {
+        if (isLeafItem(item))
+            return "";
+
+        return writeChildren(item, level);
+    }
+
+    private String writeLeafItem(MenuItem item) {
+        return "<li>" + item.getName() + "</li>";
+    }
+
+    private String writeMenuItem(MenuItem item, int level) {
         return "<li>" + item.getName() + writeChildren(item, level) + "</li>";
     }
 
     private String writeChildren(MenuItem item, int level) {
-
-        // This is a Leaf Mnu Item
-        if (item.getItems().isEmpty()) {
-            return "";
-        }
-
-        StringBuilder htmlItem = new StringBuilder();
-        htmlItem.append("<ul>");
-
-        for (MenuItem i : item.getItems()) {
-            htmlItem.append(writeItem(i, level + 1));
-        }
-
-        htmlItem.append("</ul>");
-
-        return htmlItem.toString();
+        return "<ul>" + htmlItemList(item, level) + "</ul>";
     }
 
-}
+    private String htmlItemList(MenuItem item, int level) {
+        return item.getItems().stream()
+                .map(i -> writeItem(i, level))
+                .collect(joining());
+    }
 
+    private boolean isLeafItem(MenuItem item) {
+        return item.getItems().isEmpty();
+    }
+
+
+}
