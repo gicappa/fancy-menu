1433239d618c17dde02b4b48bd91e1364d3dfeed
Gian Carlo Pace <giancarlo.pace@gmail.com>
2020-10-12 08:13:10 +0200

    Refactored the HtmlMenuWriter (3)
    Trying to reveal the intention of the code that was obscured by the append in the StreamBuffer

--- src/main/java/gk/menu/writers/HtmlMenuWriter.java
+++ src/main/java/gk/menu/writers/HtmlMenuWriter.java
@@ -7,27 +7,23 @@ public class HtmlMenuWriter implements MenuWriter {
 
     @Override
     public String writeItem(MenuItem item, int level) {
-        String result = "";
+        // This a RootMenuItem
+        if (item.getName().isBlank()) {
+            return writeChildren(item, level);
+        }
 
-        if (!item.getName().isBlank())
-            result += "<li>" + item.getName();
-
-        result += writeChildren(item, level);
-
-        if (!item.getName().isBlank())
-            result += "</li>";
-
-        return result;
-
+        // This a normal Container Menu Item or a Leaf Menu Item
+        return "<li>" + item.getName() + writeChildren(item, level) + "</li>";
     }
 
     private String writeChildren(MenuItem item, int level) {
+
+        // This is a Leaf Mnu Item
         if (item.getItems().isEmpty()) {
             return "";
         }
 
         StringBuilder htmlItem = new StringBuilder();
-
         htmlItem.append("<ul>");
 
         for (MenuItem i : item.getItems()) {
